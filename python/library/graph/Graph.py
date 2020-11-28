a88ccc04a62c4dd02cbe4152c28453bd801ba2fb
shuhei <shukuri.7336.8@gmail.com>
2020-11-28 18:36:24 +0900

    python graphライブラリ修正

--- python/library/graph/Graph.py
+++ python/library/graph/Graph.py
@@ -1,40 +1,42 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-class UnDirectedGraph:
+class Graph:
   def __init__(self, N, M):
     self.V = N
     self.E = M
     self.edge = [[] for _ in range(self.V)]
-    self.visited = [False]*N
-    self.parent = [N]*N
+    self.visited = [False]*self.V
+    self.parent = [N]*self.V
   
-  def add_edges(self, ind=1):
+  def add_edges(self, ind=1, bi=True):
     for i in range(self.E):
       a,b = map(int, input().split())
       a -= ind; b -= ind
       self.edge[a].append(b)
+      if bi:
+        self.edge[b].append(a)
 
-  def add_edge(self, a, b):
+  def add_edge(self, a, b, bi=True):
     self.edge[a].append(b)
+    if bi:
+      self.edge[b].append(a)
 
   def dfs(self, start):
     stack = deque([start])
     self.parent[start] = -1
-    self.cnts = [0]*self.V
     #記録したい値の配列を定義
     while stack:
       v = stack[-1]
       for u in self.edge[v]:
         if u==self.parent[v]:
           continue
-        if self.cnts[u]==0: #行きがけ
+        if self.parent[u]==N: #行きがけ
           self.parent[u]=v
           stack.append(u)
-          self.cnts[u] += 1
           break
-        elif self.cnts[u]==1:
-          self.cnts[u] += 1
+        else:
+          pass
           #帰りがけ処理
       else:
         stack.pop() #帰りがけまとめ
@@ -47,7 +49,7 @@ class UnDirectedGraph:
   def bfs(self, start):
     d = deque()
     self.visited[start]=True
-    d.append((start,0))
+    d.append((start, 0))
     while len(d)>0:
       v,cnt = d.popleft()
       for w in self.edge[v]:
@@ -55,3 +57,7 @@ class UnDirectedGraph:
           self.visited[w]=True
           d.append((w,cnt+1))
     return cnt
+
+N, M = map(int, input().split())
+G = Graph(N,M)
+G.add_edges(ind=1, bi=True)
--- python/library/graph/Tree.py
+++ python/library/graph/Tree.py
@@ -1,24 +1,25 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-class UnDirectedGraph:
-  def __init__(self, N, M):
+class Tree:
+  def __init__(self, N):
     self.V = N
-    self.E = M
-    self.edge = [[] for _ in range(self.V)]
-    self.visited = [False]*self.V
-    self.parent = [N]*self.V
+    self.edge = [[] for _ in range(N)]
+    self.visited = [False]*N
+    self.parent = [N]*N
   
-  def add_edges(self, ind=1):
-    for i in range(self.E):
+  def add_edges(self, ind=1, bi=True):
+    for i in range(self.V-1):
       a,b = map(int, input().split())
       a -= ind; b -= ind
       self.edge[a].append(b)
-      self.edge[b].append(a)
+      if bi:
+        self.edge[b].append(a)
 
-  def add_edge(self, a, b):
+  def add_edge(self, a, b, bi=True):
     self.edge[a].append(b)
-    self.edge[b].append(a)
+    if bi:
+      self.edge[b].append(a)
 
   def dfs(self, start):
     stack = deque([start])
@@ -49,7 +50,7 @@ class UnDirectedGraph:
   def bfs(self, start):
     d = deque()
     self.visited[start]=True
-    d.append((start, 0))
+    d.append((start,0))
     while len(d)>0:
       v,cnt = d.popleft()
       for w in self.edge[v]:
@@ -57,3 +58,7 @@ class UnDirectedGraph:
           self.visited[w]=True
           d.append((w,cnt+1))
     return cnt
+
+N = int(input())
+G = Tree(N)
+G.add_edges(ind=1, bi=True)
