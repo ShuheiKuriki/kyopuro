01c14702c0f36885078cd0e35d4527f2e810281e
shuhei <shukuri.7336.8@gmail.com>
2020-11-29 12:49:57 +0900

    リセット

--- python/B.py
+++
@@ -1,9 +0,0 @@
-N, K = map(int, input().split())
-lis = [0]*(2*N+1)
-for i in range(1,2*N+1):
-  lis[i] = N-abs(N+1-i)
-ans = 0
-for i in range(2*N+1):
-  if 0<=i+K<=2*N:
-    ans += lis[i]*lis[i+K]
-print(ans)
