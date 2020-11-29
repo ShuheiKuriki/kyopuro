fc6fdb226c365c45db3df040f57e251d5fa5253d
shuhei <shukuri.7336.8@gmail.com>
2020-11-29 20:06:15 +0900

    C++テンプレート追加

---
+++ C++/D.cpp
@@ -0,0 +1,22 @@
+// g++ D.cpp -std=c++14 -I . && ./a.out
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+// using mint = modint998244353;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, a, b) for (int i = a; i < (int)(b); i++)
+#define rrep(i, a, b) for (int i = a; i > (int)(b); i--)
+#define all(v) v.begin(), v.end()
+using ll = long long;
+const ll INF = 1e18;
+// 変数定義
+ll N, M, Q, a, b, c, d, x, y, t, T, total, cnt, ans;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
