8ad779c3d85369a155f6939e0b2aee5a347c8526
shuhei <shukuri.7336.8@gmail.com>
2021-05-23 16:17:58 +0900

    add library

---
+++ python/library/convex_hull.py
@@ -0,0 +1,36 @@
+import sys,os,io
+input = sys.stdin.readline
+#input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+N = int(input())
+A = [list(map(int, input().split())) for _ in range(N)]
+A.sort()
+
+# 正なら反時計回り、負なら時計回り
+def cross(a, b, c):
+  return (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
+
+# 上側凸包
+stack1 = [A[0],A[1]]
+i = 2
+while len(stack1)>=2 and i<=N-1:
+  if cross(stack1[-2], stack1[-1], A[i]) >= 0:
+    stack1.pop()
+  else:
+    stack1.append(A[i])
+    i += 1
+  while len(stack1)<2 and i<=N-1:
+    stack1.append(A[i])
+    i += 1
+
+# 下側凸包
+stack2 = [A[0],A[1]]
+i = 2
+while len(stack2)>=2 and i<=N-1:
+  if cross(stack2[-2], stack2[-1], A[i]) <= 0:
+    stack2.pop()
+  else:
+    stack2.append(A[i])
+    i += 1
+  while len(stack2)<2 and i<=N-1:
+    stack2.append(A[i])
+    i += 1
