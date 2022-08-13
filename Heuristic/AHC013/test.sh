8017457df49669bc032d9b1317c61609e81770c8
ShuheiKuriki <shukuri.7336.8@gmail.com>
2022-08-14 08:28:03 +0900

    ローカルテストスクリプト実装

---
+++ Heuristic/AHC013/test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+function run() {
+    file=${1}
+    seed=${2}
+    `python ${file}.py < in/${seed}.txt > out_${file}/${seed}.txt`
+}
+
+function run_multi() {
+    for i in $(seq 0 99); do
+        seed=`printf "%04d\n" "${i}"`
+        echo ${seed}
+        run ${1} ${seed}
+    done
+}
+
+function score() {
+    score=0
+    file=${1}
+    while read line
+    do
+        # echo $line
+        if [[ "$line" =~ "Score = " ]]; then
+            s=`echo ${line} | sed -r "s/Score = //"`
+            score=$((score+s))
+        fi
+    done < result_${file}.log
+    echo "${file} ${score}" | tee -a scores.log
+}
+
+function run_all() {
+    file=$1
+    run_multi ${file} >& result_${file}.log
+    score ${file}
+}
+
+if [ ${1} = "all" ];
+then
+    run_all first
+    run_all src
+else
+    echo first
+    run first ${1}
+    echo src
+    run src ${1}
+fi
