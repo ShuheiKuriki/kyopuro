28039dc546e04797186cdcae4e87f189ecef9b28
ShuheiKuriki <shukuri.7336.8@gmail.com>
2022-08-28 03:14:53 +0900

    木のデフォルト設定を双方向に変更

--- python/library/graph/Tree/tree_dijkstra.py
+++ python/library/graph/Tree/tree_dijkstra.py
@@ -10,14 +10,14 @@ class Dijkstra:
         self.V = N
         self.edge = [[] for _ in range(N)]
 
-    def add_edges(self, ind=1, bi=False):
+    def add_edges(self, ind=1, bi=True):
         for a,*A in [list(map(int, input().split())) for _ in range(self.V-1)]:
             a -= ind; b = A[0] - ind
             atob,btoa = (b,a) if len(A) == 1 else ((A[1],b),(A[1],a))
             self.edge[a].append(atob)
             if bi: self.edge[b].append(btoa)
 
-    def add_edge(self, a, b, cost=None, ind=1, bi=False):
+    def add_edge(self, a, b, cost=None, ind=1, bi=True):
         a -= ind; b -= ind
         atob,btoa = (b,a) if cost is None else ((cost,b),(cost,a))
         self.edge[a].append(atob)
