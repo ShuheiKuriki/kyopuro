f279d20bd39663080fc99cce72673dae106e9cec
ShuheiKuriki <shukuri.7336.8@gmail.com>
2022-10-21 02:19:51 +0900

    二分探索を連続値に対応

--- python/library/others/二分探索.py
+++ python/library/others/二分探索.py
@@ -1,10 +1,10 @@
-def binary_search(low, high, get_mini=True):
+def binary_search(low, high, eps=1, get_mini=True):
     #求めるのが最大値か最小値かでokとngが反転
     ok, ng = (high, low-1) if get_mini else (low, high+1)
-    mid = (ok+ng)//2
-    while abs(ok-ng)>1:
+    mid = (ok+ng)/2 if eps<1 else (ok+ng)//2
+    while abs(ok-ng)>eps:
         ok, ng = (mid, ng) if is_ok(mid) else (ok, mid)
-        mid = (ok+ng)//2
+        mid = (ok+ng)/2 if eps<1 else (ok+ng)//2
     return ok
 
 #判定問題の条件を満たすときTrueを返す関数
@@ -13,5 +13,5 @@ def is_ok(target):
 
 # 単純に下限と上限を指定
 low, high = 2, 10**18
-res = binary_search(low,high,get_mini=False)
+res = binary_search(low, high, eps=1, get_mini=False)
 print(res)
